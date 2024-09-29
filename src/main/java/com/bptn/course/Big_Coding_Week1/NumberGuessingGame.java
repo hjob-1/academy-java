5e843bb5b154cce554fd2017e21c857addf002c3
eyob <hjob12tadele@gmail.com>
2024-09-29 21:25:47 +0300

    comment added for numberGuessingGame

--- src/main/java/com/bptn/course/Big_Coding_Week1/NumberGuessingGame.java
+++ src/main/java/com/bptn/course/Big_Coding_Week1/NumberGuessingGame.java
@@ -6,26 +6,30 @@ public class NumberGuessingGame {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int targetNumber = 23;
+		int targetNumber = 23; // default value of 23
 		int guess;
 		print("Please enter a number between 1 and 100.");
+
 		do {
 			guess = scanner.nextInt();
+
+			// Input Validation
 			if (guess < 1 || guess > 100) {
 				print("Invalid input Please Enter the correct number");
-			} else if (guess == targetNumber) {
+
+			} else if (guess == targetNumber) { // compare user input with target number which is 23
 				print("Congratulations! You guessed the number correctly!");
-			} else if (guess > targetNumber) {
+
+			} else if (guess > targetNumber) { // display message based on user guess
 				print("Too high! Try again.");
 
 			} else
 
-				print("Too low! Try again.");
+				print("Too low! Try again."); // display message based on user guess
 
-		} while (guess != targetNumber);
-		{
+		} while (guess != targetNumber); // Loop will be terminated when user guess the number correctly
 
-		}
+		scanner.close(); // close the scanner object
 
 	}
 
