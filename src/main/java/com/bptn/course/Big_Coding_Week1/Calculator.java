88cb419806c2550777d53ddcdc8178bf2e7a7675
eyob <hjob12tadele@gmail.com>
2024-09-29 22:40:13 +0300

    comment added into calculator file

--- src/main/java/com/bptn/course/Big_Coding_Week1/Calculator.java
+++ src/main/java/com/bptn/course/Big_Coding_Week1/Calculator.java
@@ -12,56 +12,90 @@ public class Calculator {
 		double secondNumber;
 		double result;
 		int choice;
+		int restart = 1; // Control variable to decide whether to continue or exit
 
-		System.out.println("/…Calculator Menu…/");
-		System.out.println("Press 1 for addition");
-		System.out.println("Press 2 for subtraction");
-		System.out.println("Press 3 for multiplication");
-		System.out.println("Press 4 for division");
-		System.out.println("Press 5 to square a number");
-		System.out.println("Press 6 to find a square root");
-		System.out.println("Press 7 to find the reciprocal");
+		do {
+			calculatorMenu();
+			choice = scanner.nextInt();
 
-		choice = scanner.nextInt();
+			// Check if the choice is for basic arithmetic operations
+			if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+				System.out.print("Enter the first number: ");
+				firstNumber = scanner.nextDouble();
 
-		if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
-			System.out.print("Enter the first number: ");
-			firstNumber = scanner.nextDouble();
+				System.out.print("Enter the second number: ");
+				secondNumber = scanner.nextDouble();
 
-			System.out.print("Enter the second number: ");
-			secondNumber = scanner.nextDouble();
+				switch (choice) {
+				case 1:// Addition
+					result = firstNumber + secondNumber;
+					print("The sum of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
+					break;
+				case 2:// Subtraction
+					result = firstNumber - secondNumber;
+					print("The Subtraction of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
+					break;
+				case 3:// Multiplication
+					result = firstNumber * secondNumber;
+					print("The product of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
+					break;
+				case 4:// Division
+					result = (int) (firstNumber / secondNumber);
+					double remainder = firstNumber % secondNumber;
+					print("Dividing " + firstNumber + " by " + secondNumber + " the quotient is " + result
+							+ " The remainder is " + remainder);
+					break;
 
-			if (choice == 1) {
+				}
 
-				result = firstNumber + secondNumber;
-				System.out.print("The sum of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
-			} else if (choice == 2) {
-				result = firstNumber - secondNumber;
-				System.out.print(
-						"The Subtraction of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
-			} else if (choice == 3) {
-				result = firstNumber * secondNumber;
-				System.out.print("The Multiplication of the numbers " + firstNumber + " and " + secondNumber + " is = "
-						+ result);
-			} else if (choice == 4) {
-				result = firstNumber / secondNumber;
-				System.out.print(
-						"The Division of the numbers " + firstNumber + " and " + secondNumber + " is = " + result);
+			} else if (choice == 5 || choice == 6 || choice == 7) {
+				System.out.print("Enter Number: ");
+				number = scanner.nextInt();
+
+				switch (choice) {
+				case 5:// Square
+					result = number * number;
+					print("The Square of the number " + number + " is = " + result);
+					break;
+				case 6: // Square root
+					result = Math.sqrt(number);
+					print("The Square root of the number " + number + " is =" + result);
+					break;
+				case 7:// Reciprocal
+					if (number == 0) {
+						print("Number " + number + " Do not have a reciprocal number");
+					} else {
+						result = 1 / number;
+						print("The Reciprocal of the number " + number + " is =" + result);
+					}
+					break;
+				}
+			} else { // Handle invalid choices
+				print("Invalid choice! Please make a valid choice.");
 			}
 
-		} else {
-			System.out.print("Enter Number: ");
-			number = scanner.nextInt();
-
-			if (choice == 5) {
-				result = number * number;
-				System.out.print(" The square of a number " + number + " is = " + result);
-			}
-
-		}
+			print("To continue calculation Press 1, else press any other number to exit");
+			restart = scanner.nextInt();
+		} while (restart == 1);
 
 		scanner.close();
 
+	}
+
+	public static void print(String str) {
+		System.out.println(str);
+
+	}
+
+	public static void calculatorMenu() {
+
+		String[] menuList = { "/…Calculator Menu…/", "Press 1 for addition", "Press 2 for subtraction",
+				"Press 3 for multiplication", "Press 4 for division", "Press 5 to square a number",
+				"Press 6 to find a square root", "Press 7 to find the reciprocal" };
+
+		for (String list : menuList) {
+			print(list);
+		}
 	}
 
 }
